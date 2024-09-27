08b537de77af20c699b158143a390997c362a43e
DESKTOP-VJBKAQ0\Rompa 4TI <wiktorrompa3ti@wp.pl>
2024-09-27 10:18:14 +0200

    Zmieniłem nazwy oraz właściwości zgodnie z arkuszem

--- film/src/App.js
+++ film/src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Tytuł filmu: </label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+            <div id="emailHelp" class="form-text">Wybierz rodzaj filmu</div>
+        </div>
+        <div class="mb-3"><input type="email" class="form-control" id="film"/>
+        </div>
+        <div class="mb-3 form-check">
+            <label class="form-check-label" for="exampleCheck1"></label>
+        </div>
+        <button type="submit" class="btn btn-primary">Sprawdź</button>
+      </form>
     </div>
   );
 }
