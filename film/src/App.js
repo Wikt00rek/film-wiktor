c227dbcebdac374656bbc6fadebb35feb1debe7b
DESKTOP-VJBKAQ0\Rompa 4TI <wiktorrompa3ti@wp.pl>
2024-09-27 11:05:27 +0200

    Dodałem opcje "select"

--- film/src/App.js
+++ film/src/App.js
@@ -10,12 +10,17 @@ function App() {
           <label for="film" class="form-label">Tytuł filmu: </label>
           <input type="text" class="form-control" id="film"/>
           </div>
-            <div id="emailHelp" class="form-text">Wybierz rodzaj filmu</div>
+
         </div>
-        <div class="mb-3"><input type="email" class="form-control" id="film"/>
-        </div>
+
         <div class="mb-3 form-check">
-            <label class="form-check-label" for="exampleCheck1"></label>
+        <label for="disabledSelect" class="form-label">Wybierz rodzaj filmu </label>
+      <select id="disabledSelect" class="form-select">
+        <option>Komedia</option>
+        <option>Kryminał</option>
+        <option>Horror</option>
+        <option>Przygodowe</option>
+      </select>
         </div>
         <button type="submit" class="btn btn-primary">Sprawdź</button>
       </form>
