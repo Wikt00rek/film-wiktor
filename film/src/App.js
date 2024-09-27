21525877bd59508bbb3300b3205f203af7015144
DESKTOP-VJBKAQ0\Rompa 4TI <wiktorrompa3ti@wp.pl>
2024-09-27 11:11:42 +0200

    Cleanup

--- film/src/App.js
+++ film/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,22 +5,20 @@ function App() {
     <div className="App">
       <form>
         <div>
-        <div class="mb-3">
-          <label for="film" class="form-label">Tytuł filmu: </label>
-          <input type="text" class="form-control" id="film"/>
+          <div class="mb-3">
+            <label for="film" class="form-label">Tytuł filmu: </label>
+            <input type="text" class="form-control" id="film" />
           </div>
-
         </div>
-
         <div class="mb-3 form-check">
-        <label for="disabledSelect" class="form-label">Wybierz rodzaj filmu </label>
-      <select id="disabledSelect" class="form-select">
-        <option>Pusta</option>
-        <option>Komedia</option>
-        <option>Kryminał</option>
-        <option>Horror</option>
-        <option>Przygodowe</option>
-      </select>
+          <label for="disabledSelect" class="form-label">Wybierz rodzaj filmu </label>
+          <select id="disabledSelect" class="form-select">
+            <option>Pusta</option>
+            <option>Komedia</option>
+            <option>Kryminał</option>
+            <option>Horror</option>
+            <option>Przygodowe</option>
+          </select>
         </div>
         <button type="submit" class="btn btn-primary">Sprawdź</button>
       </form>
